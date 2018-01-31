eee63e66dc27d5768c01201b594fecab0908cbfd
lonaxous <lonaxous.game@hotmail.fr>
2018-01-31 16:33:26 +0100

    Miniconsole est devenue fonctionnel avec toutes les fonctions créés à ce jour mais le il faut réparer le cp -r

--- miniconsole.adb
+++ miniconsole.adb
@@ -25,54 +25,68 @@ End Main;
 --R1 Interpreter une commande
 Procedure InterpreteurCommande(commande: String;lcommande: integer )is
 	Ainterpreter : String(1..lcommande); --La commande à interpreter
-	LaCommande : Unbounded_String; --La commande de base à executer	
-	LeChemin : Unbounded_String;  --Le chemin désigné par la commande
-	LObjet : Unbounded_String; --L'objet que la commande va manipuler
-	Type T_Valide is array (1..3) of boolean; --Tableau 
+	Type T_US is array (1..3) of Unbounded_String; --Type de tableau pour contenir les arguments
+	T_Arguments : T_US; --Tableau contenant les arguments
 	cpt : integer; --Indice pour le tableau de booléen
-	T_Bool : T_Valide;
+	TestTiret : boolean; --Booléen pour savoir si le test du tiret est passé
 
 
 Begin
 	--R2 Analyser la commande
-	T_Bool(1) := false;
-	T_Bool(2) := false;
-	T_Bool(3) := false;
+	--R3 Initialisation des variables
 	cpt := 1;
+	TestTiret := false;
 	Ainterpreter := commande(1..lcommande);
+	--R3 Analyser mot par mot
 	for i in 1..lcommande Loop
-		If Ainterpreter(i)=' ' and cpt /= 1 then
-			T_Bool(cpt) := true;
-			cpt := cpt+1;
+		--R4 Analyser chaque caractère
+		--Erreur si trop d'argument
+		If cpt>3 then
+			Put_Line("Il y a trop d'argument");
 		Else
-			If Ainterpreter(i)='-'and not T_Bool(1)then
-				LaCommande := LaCommande & Ainterpreter(i);
-			Else
-				If not T_Bool(1) then
-					LaCommande := LaCommande & Ainterpreter(i);
-				Else
-					If not T_Bool(2) then
-						LObjet := LObjet & Ainterpreter(i);
-					Else
-						If not T_Bool(3) then
-							LeChemin := LeChemin & Ainterpreter(i);
-						Else
-							T_Bool(cpt):= true;
-							cpt := cpt + 1;
-						End If;
-					End if;
-				End If;
+			--On vérifie le tiret
+			If Ainterpreter(i)='-' and cpt = 1 then
+				TestTiret := false;
+				T_Arguments(cpt) := T_Arguments(cpt) & Ainterpreter(i);
+			--Si il y a un espace après le premier argument on regarde s'il y a un tiret
+			Elsif  Ainterpreter(i)=' ' and cpt = 1 and not TestTiret then
+				TestTiret:=true;
+			--Si ce n'est pas un tiret,c 'est qu'il n'y en a pas on passe à l'argument suivant
+			Elsif Ainterpreter(i)/='-' and cpt = 1 and TestTiret then
+				cpt := cpt + 1;
+				TestTiret:=false;
+				T_Arguments(cpt):= T_Arguments(cpt) & Ainterpreter(i);
+			Elsif Ainterpreter(i)=' 'then
+				cpt := cpt +1;
+			--Sinon on rajoute des caractères dans les arguments
+	 		Else
+	 			TestTiret:=false;
+				T_Arguments(cpt) := T_Arguments(cpt) & Ainterpreter(i);
 			End If;
 		End If;
 	End Loop;
 
 	--R2 Executer la commande
-	If LaCommande = "ls" then
+	If T_Arguments(1) = "ls" then
 		Ls;
-	Elsif LaCommande = "pwd" then
+	Elsif T_Arguments(1) = "pwd" then
 		Pwd;
-	Elsif LaCommande = "ls -r" then
+	Elsif T_Arguments(1) = "ls-r" then
 		LsR;
+	Elsif T_Arguments(1) = "touch" then
+		Touch(to_string(T_Arguments(2)));
+	Elsif T_Arguments(1) = "mkdir" then
+		Mkdir(to_string(T_Arguments(2)));
+	Elsif T_Arguments(1) = "rm" then
+		Rm(to_string(T_Arguments(2)));
+	Elsif T_Arguments(1) = "rm-r" then
+		RmR(to_string(T_Arguments(2)));
+	Elsif T_Arguments(1) = "mv" then
+		Mv(to_string(T_Arguments(2)),to_string(T_Arguments(3)));
+	Elsif T_Arguments(1) = "cd" then
+		Cd(to_string(T_Arguments(2)));
+	Elsif T_Arguments(1) = "cp-r" then
+		CpR(to_string(T_Arguments(2)),to_string(T_Arguments(3))); 
 	Else
 		Put_Line("Commande inexistante !");
 	End If;
